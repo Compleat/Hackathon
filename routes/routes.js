c60c2cbcf296a776063deaa81236bf67f6b9330b
derekelec <mathewson17@live.ca>
2017-02-16 15:47:21 -0800

    bad model

--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,13 @@ var models = require('../models');
 var User = models.User;
 const yelp = require('yelp-fusion');
 
-var yelp = new Yelp({
-  clientId: process.env.CONSUMER_KEY,
-  clientSecret: process.env.CONSUMER_SECRET,
-});
+const clientId=process.env.YELP_CONSUMER_KEY;
+const clientSecret=process.env.YELP_CONSUMER_SECRET;
+
+const searchRequest = {
+  term:'Four Barrel Coffee',
+  location: 'san francisco, ca'
+};
 
 yelp.accessToken(clientId, clientSecret).then(response => {
   const client = yelp.client(response.jsonBody.access_token);
@@ -16,22 +19,23 @@ yelp.accessToken(clientId, clientSecret).then(response => {
     const firstResult = response.jsonBody.businesses[0];
     const prettyJson = JSON.stringify(firstResult, null, 4);
     console.log(prettyJson);
+    console.log('hello')
     var rest = new models.Restaurant({
       restname: prettyJson.name,
-      restlocation: {
-        display_address: prettyJson.location.display_address,
-        zip_code: prettyJson.location.zip_code,
-        city: prettyJson.location.city,
-        address1: prettyJson.location.address1,
-        state: prettyJson.location.state,
-        country: prettyJson.location.country
-      },
+      // restlocation: {
+      //   display_address: prettyJson.location.display_address,
+      //   zip_code: prettyJson.location.zip_code,
+      //   city: prettyJson.location.city,
+      //   address1: prettyJson.location.address1,
+      //   state: prettyJson.location.state,
+      //   country: prettyJson.location.country
+      // },
       restdistance: prettyJson.distance,
       restcategory: prettyJson.categories,
-      restcoord: {
-        restlat: prettyJson.coordinates.latitude,
-        restlng: prettyJson.coordinates.longitude
-      },
+      // restcoord: {
+      //   restlat: prettyJson.coordinates.latitude,
+      //   restlng: prettyJson.coordinates.longitude
+      // },
       restrating: prettyJson.rating,
       restphone: prettyJson.phone,
       restprice: prettyJson.price,
@@ -39,6 +43,7 @@ yelp.accessToken(clientId, clientSecret).then(response => {
       restcount: prettyJson.review_count,
       resurl: prettyJson.url
         });
+        console.log(rest);
     rest.save(function(err){
       if(err) throw new Error ('LISA SUCKS');
     })
@@ -53,7 +58,7 @@ res.render('signup');
 });
 
 router.post('/signup', function(res,res){
-  
+
 });
 
 router.get('/', function(req, res, next) {
